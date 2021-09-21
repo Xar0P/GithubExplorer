6646dc88905c2edcef4b70c8882e6e674b480901
Pietro <pietroricardocres@hotmail.com>
2021-09-21 09:58:14 -0300

    babel & webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.jsx'), // Arquivo padrão/entrada
+    output: { // Identificando que arquivo vai ser retornado
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: { // Que arquivos ele vai transformar
+        extensions: [
+            '.js', '.jsx'
+        ]
+    },
+    module: { // Como a aplicação vai se comportar com os arquivos
+        rules: [
+            {
+                test: /\.js?x$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ],
+    }
+};
